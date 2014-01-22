dab175f91ed8fa444dc919aca28a8751c2d7143c
Wilge <ruta.lukasz@gmail.com>
2014-01-22 22:26:34 +0100

    1st full comunication for one use case

--- src/sag/Bankomat.java
+++ src/sag/Bankomat.java
@@ -1,11 +1,10 @@
 package sag;
 
-import java.util.Vector;
+import java.util.StringTokenizer;
 
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
-import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPANames;
@@ -20,9 +19,79 @@ public class Bankomat extends Agent
 {
 
 	AID Serwer = new AID("Serwer", AID.ISLOCALNAME);
-	String ostatniKlient;
+	String ostatniKlient; //ID karty
 	String ostatniaKwota;
+	Integer iloscBankomatow=9; 
+	Integer iloscOdpowiedzi=0;
+	Integer odpowiedzTak=0;
+	Integer autoryzacja=0;
+	String IDkarty;
+	String Kwota;
+	String Poprzednia_kwota;
+	String PIN;
+	String wiadomosc;
 	
+
+
+	protected void parsuj(String content)
+	{
+		
+		try{
+		if (content.isEmpty())// equals(null))
+		{ 
+			System.out.println("Brak treœci wiadomoœci!");
+		}
+		else {		
+		{
+			StringTokenizer st = new StringTokenizer(content, ";");
+			System.out.println("Liczba tokenow: "+st.countTokens());
+			switch (st.countTokens())
+			{
+			case 2:
+				setIDkarty(st.nextToken());
+				setPoprzednia_kwota(st.nextToken());
+				break;
+			case 3:
+				setIDkarty(st.nextToken());
+				setPoprzednia_kwota(st.nextToken());
+				String odpowiedz = st.nextToken();
+				if (odpowiedz.equals("TAK")) setOdpowiedzTak(getOdpowiedzTak()+1);	
+				else if (odpowiedz.equals("NIE"));
+				else System.out.println("B³êdna odpowiedz");			
+				break;
+			case 4:
+				setIDkarty(st.nextToken());
+				setKwota(st.nextToken());
+				setPoprzednia_kwota(st.nextToken());
+				setPIN(st.nextToken());
+				break;
+			default:
+				System.out.println("B³êna treœæ wiadomoœci!");					
+			}
+		}
+		}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		//parsuj contontent i zapisz do 4 zmiennych: IDkarty; Kwota; Poprzednia_kwota; PIN				
+		//ID karty - potrzebne do bankomtów i do serwera
+		//Kwota - wysy³ane do serwera
+		//Poprzednia_kwota - potrzebna bankomatom do sprawdzenia
+		//PIN - sprawdzany na serwerze
+		//"1010";150;0;"0011"
+		
+	//TODO parser wiadomosci, zapisywanie zmiennych
+		//wiadomosci typu: "Id_karty;Kwota;Poprzednia_kwota;PIN"  	//args 4
+		//wiadomosci typu: "Id_karty;Poprzednia_kwota"				//args 2
+		//wiadomosci typu: "Id_karty;Poprzednia_kwota;TAK/NIE"		//args 3
+		
+		//ID_karty;Poprzednia_kwota;Kwota;PIN;Tak/Nie
+	}
+	
+
 	protected void setup()
 	{
 		String serviceName = "cash withdrawn";
@@ -33,7 +102,7 @@ public class Bankomat extends Agent
 		{
 			serviceName = (String) args[0];
 		}
-
+		
 		// Register the service
 		System.out.println("Agent " + getLocalName()
 				+ " registering service \"" + serviceName
@@ -70,9 +139,9 @@ public class Bankomat extends Agent
 		
 		System.out.println("Witaj, nazywam sie " + getAID().getLocalName());
 		System.out.println("Moj AID to: " + getAID());
+
 		
-		
-	addBehaviour(new Behaviour()
+	addBehaviour(new Behaviour() //zachowanie 1 - reakcja na chêæ wyp³aty gotówki
 		{ // przepytaj bankomaty - DF services //done
 		// if chec wyplaty gotowki //done
 		// zapytaj DF o inne bankomaty //done
@@ -80,8 +149,10 @@ public class Bankomat extends Agent
 
 			public void action()
 			{	
-				ACLMessage msg_wyplata = receive();
-
+				MessageTemplate mt;
+				mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
+				ACLMessage msg_wyplata = receive(mt);				
+				//wiadomosci typu: "Id_karty;kwota;Poprzednia_kwota;PIN"
 				if (msg_wyplata != null	&& msg_wyplata.getPerformative() == ACLMessage.REQUEST)
 			{
 				DFAgentDescription dfd = new DFAgentDescription();
@@ -90,9 +161,18 @@ public class Bankomat extends Agent
 				dfd.addServices(sd);
 				SearchConstraints ALL = new SearchConstraints();
 				ALL.setMaxResults(new Long(-1));
-		
-				
 
+				parsuj(msg_wyplata.getContent());
+				setWiadomosc(msg_wyplata.getContent());
+
+				System.out.println("Nr karty to: " +getIDkarty());
+				System.out.println("Kwota to: " +getKwota());
+				System.out.println("Poprzednia kwota to:" +getPoprzednia_kwota());
+				System.out.println("Pin: " + getPIN());
+				System.out.println("Liczba TAK: "+getOdpowiedzTak());
+				System.out.println();
+
+				
 				try
 				{
 					DFAgentDescription[] result = DFService.search(myAgent, dfd, ALL);
@@ -103,14 +183,17 @@ public class Bankomat extends Agent
 				                System.out.println(bankomat[i]);							
 								
 									ACLMessage askNeighbors = new ACLMessage(ACLMessage.REQUEST_WHEN);
+									//wiadomosci typu: "Id_karty;Poprzednia_kwota"
 									askNeighbors.setContent(msg_wyplata.getContent()); // TODO zmienic content
 									askNeighbors.addReceiver(bankomat[i]);  
 									send(askNeighbors);
 									System.out.println("Wykonano.");
 									                
 				        }
-					System.out.println("Iloœæ bankomatów: " +bankomat.length );
+					iloscBankomatow = bankomat.length;
+					System.out.println("Iloœæ bankomatów: " +iloscBankomatow );
 					System.out.println("Zaktualizowano listê bankomatów.");
+				
 				}
 				catch (FIPAException fe)
 				{ fe.printStackTrace(); }
@@ -128,7 +211,7 @@ public class Bankomat extends Agent
 
 		});
 
-	addBehaviour(new Behaviour()
+	addBehaviour(new Behaviour() //zachowanie 2 - reakcja na zapytanie bankomatu
 		{ // odpowiedz na zapytanie bankomatu
 		// if zapytanie o historie transakcji
 		// Czy ID#### wyplacal u was ostatnio pieniadze?
@@ -136,15 +219,25 @@ public class Bankomat extends Agent
 			@Override
 			public void action()
 			{
-				ACLMessage msg_pytanieBankomatu = receive();
+				MessageTemplate mt;
+				mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST_WHEN);	
+				ACLMessage msg_pytanieBankomatu = receive(mt);
 				if (msg_pytanieBankomatu != null	&& msg_pytanieBankomatu.getPerformative() == ACLMessage.REQUEST_WHEN)
 				{
 					//TODO sprawdz czy tu z tej karty wyp³acano ostatnio pieni¹dze
+					
+					//*************************************************************//
+					//TODO potrzebujemy informacji o kwocie ostatniej transakcji!!!//
+					//*************************************************************//
+					
+					//wiadomosci typu: "Id_karty;Poprzednia_kwota;TAK/NIE"
 					ACLMessage replyNeighbor = new ACLMessage(ACLMessage.INFORM);
-					replyNeighbor.setContent(msg_pytanieBankomatu.getContent()); // TODO zmienic content
+					replyNeighbor.setContent(msg_pytanieBankomatu.getContent()); // TODO zmienic content - odpowiedz czy wyplacano
 					replyNeighbor.addReceiver(msg_pytanieBankomatu.getSender());  
 					send(replyNeighbor);
 					System.out.println("Odpowiedziano na zapytanie bankomatu.");
+					System.out.println("Biezaca liczba bankomatów: "+iloscBankomatow);
+
 				}
 				else
 					block();
@@ -154,13 +247,12 @@ public class Bankomat extends Agent
 			@Override
 			public boolean done()
 			{
-				// TODO Auto-generated method stub
 				return false;
 			}
 
 		});
 
-	addBehaviour(new Behaviour()
+	addBehaviour(new Behaviour() //zachowanie 3 - reakcja na odpowiedz od wszystkich bankomatów
 		{ // odpowiedz od bankomatu
 		// if wszystkie bankomaty odpowiedzialy (albo okreslona ilosc)
 		// if uplynal czas
@@ -171,37 +263,50 @@ public class Bankomat extends Agent
 			@Override
 			public void action()
 			{
-				// TODO Auto-generated method stub
-				block();
+				if (iloscBankomatow == iloscOdpowiedzi)
+				{
+					//TODO sprawdzanie warunków
+					iloscOdpowiedzi = 0;
+					System.out.println("Wszystkie bankomaty odpowiedzia³y. Iloœæ bankomatów: "+iloscBankomatow);
+//					if (getOdpowiedzTak()==1) // || odpowiedzTak==0 && kartaPusta==1)
+					{
+						autoryzacja=1;
+						System.out.println("autoryzacja : "+autoryzacja);
+					}
+//					else autoryzacja=-1;						
+				}				
 			}
 
 			@Override
 			public boolean done()
 			{
-				// TODO Auto-generated method stub
 				return false;
 			}
 
 		});
 
-	addBehaviour(new Behaviour()
-		{ // zapytaj serwer o autoryzacje
-		// if flaga zgodnosci
+	addBehaviour(new Behaviour() //zachowanie 4 - sprawdzanie odpowiedzi od bankomatów
+		{  //TODO to zachowanie nie jest kierowane do serwera, tutaj zliczamy odpowiedzi
+		// if flaga zgodnosci //TODO ustawiamy flagê na podstawie odpowiedzi od bankomatów
 
 			@Override
 			public void action()
 			{
-				ACLMessage msg_rec = receive();
-				// TODO zmiena flagi z request
-				// if flag == REQUEST then
-				if (msg_rec != null
-						&& msg_rec.getPerformative() == ACLMessage.INFORM)
+				MessageTemplate mt;
+				mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);	
+				ACLMessage msg_rec = receive(mt);
+				if (msg_rec != null	&& msg_rec.getPerformative() == ACLMessage.INFORM)
 				{
-					ACLMessage confirm = new ACLMessage(ACLMessage.REQUEST);
-					confirm.setContent(msg_rec.getContent());
-					confirm.addReceiver(Serwer);
-					send(confirm);
-					System.out.println("Wykonano.");
+					if (getOdpowiedzTak()==0)
+					{
+						//TODO  if ktorys bankomat odpowiedzial tak -> odpowiedzTak=1; 						
+					}
+					else if (getOdpowiedzTak()==1)
+					{
+						//TODO if kolejny odpowiedzial Tak -> oszustwo -> odpowiedzTak=2;
+					}
+					iloscOdpowiedzi++;
+					System.out.println("Biezaca liczba odpowiedzi: "+iloscOdpowiedzi);
 				} else
 					block();
 			}
@@ -209,31 +314,152 @@ public class Bankomat extends Agent
 			@Override
 			public boolean done()
 			{
-				// TODO Auto-generated method stub
 				return false;
 			}
 		});
+	
+	addBehaviour(new Behaviour() //zachowanie 5 - wyslanie zapytania do serwera
+	{ // zapytaj serwer o autoryzacje 
+	// wyswietl komunikat
+		@Override
+		public void action()
+		{
+			// TODO Auto-generated method stub
+			if (autoryzacja==1)// && iloscOdpowiedzi==iloscBankomatow)
+			{
+			ACLMessage confirm = new ACLMessage(ACLMessage.REQUEST);
+			//wiadomosci typu: "Id_karty;kwota;Poprzednia_kwota;PIN"
+			confirm.setContent(wiadomosc);
+			confirm.addReceiver(Serwer);
+			send(confirm);
+			System.out.println("Wykonano.");
+			autoryzacja=0; 
+			}
+			else if (autoryzacja==-1)
+			{
+				System.out.println("Wykryto próbê oszustwa, brak po³¹czenia do serwera.");
+			}
+		}
 
-	addBehaviour(new Behaviour()
+		@Override
+		public boolean done()
+		{
+			return false;
+		}
+
+	});
+
+	addBehaviour(new Behaviour() //zachowanie 6 - reakcja na odpowiedz od serwera
 		{ // odpowiedz od serwera
 		// wyswietl komunikat
-		// if autoryzacja => wyplac pieniadze
+		// if autoryzacja od serwera => wyplac pieniadze
 			@Override
 			public void action()
 			{
-				// TODO Auto-generated method stub
-				block();
+				MessageTemplate mt;
+				mt = MessageTemplate.MatchSender(Serwer);	
+				ACLMessage msg_rec = receive(mt);
+				if (msg_rec != null)
+				{
+					System.out.println("Otrzymano odpowiedz od serwera.");
+				} else
+					block();
 			}
 
 			@Override
 			public boolean done()
 			{
-				// TODO Auto-generated method stub
 				return false;
 			}
 
 		});
-
+	
+	
+	
+	
+	
 	}
 
+	public Integer getOdpowiedzTak()
+	{
+		return odpowiedzTak;
+	}
+
+	public void setOdpowiedzTak(Integer odpowiedzTak)
+	{
+		this.odpowiedzTak = odpowiedzTak;
+	}
+	public String getOstatniKlient()
+	{
+		return ostatniKlient;
+	}
+
+	public void setOstatniKlient(String ostatniKlient)
+	{
+		this.ostatniKlient = ostatniKlient;
+	}
+
+	public String getOstatniaKwota()
+	{
+		return ostatniaKwota;
+	}
+
+	public void setOstatniaKwota(String ostatniaKwota)
+	{
+		this.ostatniaKwota = ostatniaKwota;
+	}
+
+	public String getIDkarty()
+	{
+		return IDkarty;
+	}
+
+	public void setIDkarty(String iDkarty)
+	{
+		IDkarty = iDkarty;
+	}
+
+	public String getKwota()
+	{
+		return Kwota;
+	}
+
+	public void setKwota(String kwota)
+	{
+		Kwota = kwota;
+	}
+
+	public String getPoprzednia_kwota()
+	{
+		return Poprzednia_kwota;
+	}
+
+	public void setPoprzednia_kwota(String poprzednia_kwota)
+	{
+		Poprzednia_kwota = poprzednia_kwota;
+	}
+
+	public String getPIN()
+	{
+		return PIN;
+	}
+
+	public void setPIN(String pIN)
+	{
+		PIN = pIN;
+	}
+
+
+	public String getWiadomosc()
+	{
+		return wiadomosc;
+	}
+
+
+	public void setWiadomosc(String wiadomosc)
+	{
+		this.wiadomosc = wiadomosc;
+	}
+
+	
 }
--- src/sag/Serwer.java
+++ src/sag/Serwer.java
@@ -1,183 +1,247 @@
 package sag;
-import jade.core.AID;
+
+//import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
-import jade.core.behaviours.OneShotBehaviour;
-import jade.core.behaviours.ReceiverBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+import java.io.BufferedWriter;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-import java.util.Vector;
 
-public class Serwer extends Agent{
+public class Serwer extends Agent
+{
 
 	ArrayList<ArrayList<String>> dataArray = new ArrayList<>();
 	String IDkarty;
-	String Kwota;
-	String Poprzednia_kwota;
+	int Kwota;
+	int Poprzednia_kwota;
 	String PIN;
-	
-	
+
+	// "ID_karty";"PIN";"Ostatnia_transakcja_kwota";"Nieudane_autoryzacje";"Licznik_bezgotówkowych";"Saldo";"Wlasciciel"
+	String tID, tPIN, tWLASCICIEL;
+	int tOSTATNIA, tNIEAUTORYZOWANE, tBEZGOTOWKOWE, tSALDO;
+
+	String tworz_wiadomosc(String id, String pin, int ost, int nie, int bez,
+			int sal, String wla)
+	{
+		return (id + ";" + pin + ";" + ost + ";" + nie + ";" + bez + ";" + sal
+				+ ";" + wla);
+	}
+
+	// void czytaj_wiadomosc(String content)
+	// {
+	// try{
+	// if (content.isEmpty())
+	// {
+	// System.out.println("Brak treœci wiadomoœci!");
+	// }
+	// else {
+	// {
+	// StringTokenizer st = new StringTokenizer(content, ";");
+	// }
+	// }
+
 	protected void parsuj(String content)
 	{
-		
-		try{
-		if (content.isEmpty())// equals(null))
-		{ 
-			System.out.println("Brak treœci wiadomoœci!");
-		}
-		else {		
+
+		try
 		{
-			StringTokenizer st = new StringTokenizer(content, ";");
-			System.out.println("Liczba tokenow: "+st.countTokens());
-			switch (st.countTokens())
+			if (content.isEmpty())
 			{
-			case 2:
-				setIDkarty(st.nextToken());
-				setPoprzednia_kwota(st.nextToken());
-				break;
-			case 3:
-				setIDkarty(st.nextToken());
-				setPoprzednia_kwota(st.nextToken());
-				String odpowiedz = st.nextToken();
-				//if (odpowiedz.equals("TAK")) setOdpowiedzTak(getOdpowiedzTak()+1);	
-				//else if (odpowiedz.equals("NIE"));
-				//else System.out.println("B³êdna odpowiedz");			
-				break;
-			case 4:
-				setIDkarty(st.nextToken());
-				setKwota(st.nextToken());
-				setPoprzednia_kwota(st.nextToken());
-				setPIN(st.nextToken());
-				break;
-			default:
-				System.out.println("B³êna treœæ wiadomoœci!");					
+				System.out.println("Brak treœci wiadomoœci!");
+			} else
+			{
+				{
+					StringTokenizer st = new StringTokenizer(content, ";");
+					// System.out.println("Liczba tokenow: "+st.countTokens());
+					switch (st.countTokens())
+					{
+					case 2:
+						setIDkarty(st.nextToken());
+						setPoprzednia_kwota(Integer.parseInt(st.nextToken()));
+						break;
+					case 3:
+						setIDkarty(st.nextToken());
+						setPoprzednia_kwota(Integer.parseInt(st.nextToken()));
+						String odpowiedz = st.nextToken();
+						// if (odpowiedz.equals("TAK"))
+						// setOdpowiedzTak(getOdpowiedzTak()+1);
+						// else if (odpowiedz.equals("NIE"));
+						// else System.out.println("B³êdna odpowiedz");
+						break;
+					case 4:
+						setIDkarty(st.nextToken());
+						setKwota(Integer.parseInt(st.nextToken()));
+						setPoprzednia_kwota(Integer.parseInt(st.nextToken()));
+						setPIN(st.nextToken());
+						break;
+					default:
+						System.out.println("B³êna treœæ wiadomoœci!");
+					}
+				}
 			}
-		}
-		}
-		}
-		catch (Exception e)
+		} catch (Exception e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public void setup() 
-	{
-		//otworz plik
-	
-		String path = ("C:/Users/luky/Desktop/sag.csv");
-		
-		//wczytaj kolejne linie do tablicy	
-		dataArray = FileLoader.loadFile(path);
-		
-		System.out.println("Rozmiar arraylisty: " + dataArray.size() + " x " + dataArray.get(0).size());
 
-		
-		for (int nr=0 ; nr < dataArray.size() ; nr++ )			
+	public void setup()
+	{
+		// otworz plik
+
+		String path = ("C:/Users/luky/Desktop/sag.csv");
+
+		// wczytaj kolejne linie do tablicy
+		dataArray = FileLoader.loadFile(path);
+
+		System.out.println("Rozmiar arraylisty: " + dataArray.size() + " x "
+				+ dataArray.get(0).size());
+
+		for (int nr = 1; nr < dataArray.size(); nr++)
 		{
-			System.out.println ("Zdarzenie nr " + nr + ":");
-			for (int i=0 ; i<dataArray.get(nr).size() ; i++)
-				System.out.print(dataArray.get(nr).get(i)+ "|");
+			System.out.println("Zdarzenie nr " + nr + ":");
+			for (int i = 0; i < dataArray.get(nr).size(); i++)
+				System.out.print(dataArray.get(nr).get(i) + "|");
 			System.out.println();
 		}
-        System.out.println();
-				       		
-        		
+		System.out.println();
+
+		System.out.println("**********************");
+		System.out.println(dataArray.get(18).toString());
+		// ID_karty|PIN|Ostatnia_transakcja_kwota|Nieudane_autoryzacje|Saldo|Wlasciciel|
+
 		addBehaviour(new Behaviour()
-		{			
+		{
 			@Override
 			public void action()
 			{
-				
-				//otrzymujemy aclm
-				//wiadomosci typu: "Id_karty;Kwota;Poprzednia_kwota;PIN"  	//args 4
-				//1008;100;0;"0009"
-				//sprawdzamy czy content wiadomosci zgadza sie z ponizszymi wartosciami			
-				
-				
 
+				// otrzymujemy aclm
+				// wiadomosci typu: "Id_karty;Kwota;Poprzednia_kwota;PIN" //args
+				// 4
+				// 1008;100;0;"0009"
+				// sprawdzamy czy content wiadomosci zgadza sie z ponizszymi
+				// wartosciami
 
-
-				//agree() , zeruj licznik, odejmij saldo
-				//reject(), inkrementuj licznik nieautoryzowanych
+				// agree() , zeruj licznik, odejmij saldo
+				// reject(), inkrementuj licznik nieautoryzowanych
 
 				// TODO Auto-generated method stub
 				MessageTemplate mt;
-				mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);	
+				mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
 				ACLMessage msg_rec = receive(mt);
-				if (msg_rec != null	&& msg_rec.getPerformative() == ACLMessage.INFORM)
+				if (msg_rec != null
+						&& msg_rec.getPerformative() == ACLMessage.REQUEST)
 				{
-				//wiadomosci typu: "Id_karty;kwota;Poprzednia_kwota;PIN"
+					// wiadomosci typu: "Id_karty;kwota;Poprzednia_kwota;PIN"
 					parsuj(msg_rec.getContent());
-				
-				System.out.println("Odebrano wiadomoœæ od: " +msg_rec.getSender());
-				
-				int rekord = 0;
-				for (int i=0; i< dataArray.size(); i++)
-				if (dataArray.get(i).contains(IDkarty)) 
-				{
-					rekord = i;
-					System.out.println("Zdarzenie o polu Id: "+IDkarty+" ma numer: " + rekord);
-				}
-				
-//				if licznik<3
-				int Liczba_nieautoryzowanych = Integer.parseInt(dataArray.get(rekord).get(3));
-				if (Liczba_nieautoryzowanych > 2) 
-				System.out.println("Konto zablokowane!");
-				else
-				{
-//					if pin=pin_wzor
-					if (!PIN.equals(dataArray.get(rekord).get(1)))
-					{
-					System.out.println("B³êdny kod PIN!");					
-					Liczba_nieautoryzowanych++;
-					System.out.println("Pozosta³o " + (3-Liczba_nieautoryzowanych) + " prob.");
-					}
+
+					System.out.println("Odebrano wiadomoœæ od: "
+							+ msg_rec.getSender());
+
+					int rekord = 0;
+					for (int i = 1; i < dataArray.size(); i++)
+						if (dataArray.get(i).contains(IDkarty))
+						{
+							rekord = i;
+							System.out.println("Zdarzenie o polu Id: "
+									+ IDkarty + " ma numer: " + rekord);
+						}
+
+					if (rekord == 0)
+						System.out.println("Nie znaleziono podanego IDkarty!");
 					else
 					{
-//						if kwota<saldo
-						if (Integer.parseInt(Kwota)>Integer.parseInt(dataArray.get(rekord).get(4)))
-						{
-							System.out.println("Brak srodków!");
-						}
-						else 
-						{
-//							if ost_transakcja sie zgadza
-							if (!Poprzednia_kwota.equals(dataArray.get(rekord).get(2)))
-								System.out.println("Poprzednia transakcja ró¿na, wykryto próbê oszustwa!");
-							else
-							{
-								//agree() , zeruj licznik, odejmij saldo
-								Liczba_nieautoryzowanych = 0;
-								//odejmij saldo
-								//wyslij alcm do bankomatu
-								System.out.println("Wyplacono pieniadze.");
-								
-				
-//				String ID_karty = dataArray.get(1).get(0);
-//				String Wlasciciel = dataArray.get(1).get(5);
-					}
+						// tID, tPIN, tOSTATNIA, tNIEAUTORYZOWANE,
+						// tBEZGOTOWKOWE, tSALDO, tWLASCICIEL;
+						settID(dataArray.get(rekord).get(0));
+						settPIN(dataArray.get(rekord).get(1));
+						settOSTATNIA(Integer.parseInt(dataArray.get(rekord)
+								.get(2)));
+						settNIEAUTORYZOWANE(Integer.parseInt(dataArray.get(
+								rekord).get(3)));
+						settBEZGOTOWKOWE(Integer.parseInt(dataArray.get(rekord)
+								.get(4)));
+						settSALDO(Integer
+								.parseInt(dataArray.get(rekord).get(5)));
+						settWLASCICIEL(dataArray.get(rekord).get(6));
 
-				}
-				}
-				}
+						// if licznik<3
+						if (tNIEAUTORYZOWANE > 2)
+							System.out.println("Konto zablokowane!");
+						else
+						{
+							if (Kwota < 50)
+								settBEZGOTOWKOWE(gettBEZGOTOWKOWE() + 1);
+							// if pin=pin_wzor
+							else if (!PIN.equals(dataArray.get(rekord).get(1)))
+							{
+								System.out.println("B³êdny kod PIN!");
+								settNIEAUTORYZOWANE(gettNIEAUTORYZOWANE() + 1);
+								System.out.println("Pozosta³o "
+										+ (3 - gettNIEAUTORYZOWANE())
+										+ " prob.");
+							} else
+							{
+								settBEZGOTOWKOWE(0);
+								// if kwota<saldo
+								if (Kwota > tSALDO)
+								{
+									System.out.println("Brak srodków!");
+								} else
+								{
+									// if ost_transakcja sie zgadza
+									if (getPoprzednia_kwota() != gettOSTATNIA())
+										System.out
+												.println("Poprzednia transakcja ró¿na, wykryto próbê oszustwa!");
+									else
+									{
+										// agree() , zeruj licznik, odejmij
+										// saldo
+										settNIEAUTORYZOWANE(0);
+										// odejmij saldo
+										settSALDO(gettSALDO() - getKwota());
+
+										System.out
+												.println("Wyplacono pieniadze.");
+
+										// wyslij alcm do bankomatu
+										System.out.println("Wiadomosc:");
+										String odpowiedz=(tworz_wiadomosc(
+												gettID(), gettPIN(),
+												gettOSTATNIA(),
+												gettNIEAUTORYZOWANE(),
+												gettBEZGOTOWKOWE(),
+												gettSALDO(), gettWLASCICIEL()));
+										System.out.println(odpowiedz);
+										
+										ACLMessage reply = new ACLMessage(1);
+										reply.setContent(odpowiedz); // TODO zmienic content - odpowiedz czy wyplacano
+										reply.addReceiver(msg_rec.getSender());  
+										send(reply);
+
+									}
+
+								}
+							}
+						}
+					}
 				}
 			}
-			
+
 			@Override
 			public boolean done()
 			{
 				// TODO Auto-generated method stub
 				return false;
 			}
-		}); 
+		});
 	}
 
-	
 	public String getIDkarty()
 	{
 		return IDkarty;
@@ -188,22 +252,22 @@ public class Serwer extends Agent{
 		IDkarty = iDkarty;
 	}
 
-	public String getKwota()
+	public int getKwota()
 	{
 		return Kwota;
 	}
 
-	public void setKwota(String kwota)
+	public void setKwota(int kwota)
 	{
 		Kwota = kwota;
 	}
 
-	public String getPoprzednia_kwota()
+	public int getPoprzednia_kwota()
 	{
 		return Poprzednia_kwota;
 	}
 
-	public void setPoprzednia_kwota(String poprzednia_kwota)
+	public void setPoprzednia_kwota(int poprzednia_kwota)
 	{
 		Poprzednia_kwota = poprzednia_kwota;
 	}
@@ -216,6 +280,76 @@ public class Serwer extends Agent{
 	public void setPIN(String pIN)
 	{
 		PIN = pIN;
+	}
+
+	public String gettID()
+	{
+		return tID;
+	}
+
+	public void settID(String tID)
+	{
+		this.tID = tID;
+	}
+
+	public String gettPIN()
+	{
+		return tPIN;
+	}
+
+	public void settPIN(String tPIN)
+	{
+		this.tPIN = tPIN;
+	}
+
+	public int gettOSTATNIA()
+	{
+		return tOSTATNIA;
+	}
+
+	public void settOSTATNIA(int tOSTATNIA)
+	{
+		this.tOSTATNIA = tOSTATNIA;
+	}
+
+	public int gettNIEAUTORYZOWANE()
+	{
+		return tNIEAUTORYZOWANE;
+	}
+
+	public void settNIEAUTORYZOWANE(int tNIEAUTORYZOWANE)
+	{
+		this.tNIEAUTORYZOWANE = tNIEAUTORYZOWANE;
+	}
+
+	public int gettBEZGOTOWKOWE()
+	{
+		return tBEZGOTOWKOWE;
+	}
+
+	public void settBEZGOTOWKOWE(int tBEZGOTOWKOWE)
+	{
+		this.tBEZGOTOWKOWE = tBEZGOTOWKOWE;
+	}
+
+	public int gettSALDO()
+	{
+		return tSALDO;
+	}
+
+	public void settSALDO(int tSALDO)
+	{
+		this.tSALDO = tSALDO;
+	}
+
+	public String gettWLASCICIEL()
+	{
+		return tWLASCICIEL;
+	}
+
+	public void settWLASCICIEL(String tWLASCICIEL)
+	{
+		this.tWLASCICIEL = tWLASCICIEL;
 	}
 
 }
